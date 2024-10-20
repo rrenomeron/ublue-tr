5af1d2bf8e4e1c37826e14626489167b710bc794
Rich Renomeron <rrenomeron@gmail.com>
2024-10-20 14:35:58 -0400

    fix: temp workaround for missing gnome-terminal in F41 bluefin

--- files/scripts/bluefin-fix-hidden-apps.sh
+++ files/scripts/bluefin-fix-hidden-apps.sh
@@ -5,7 +5,9 @@ set -eou pipefail
 # Bluefin hides Gnome Terminal and System Monitor by default, since they
 # ship replacements for them.  Those replacements don't work for us, so
 # we bring back the OG apps
-sed -i '/NoDisplay=true/d' /usr/share/applications/org.gnome.Terminal.desktop
+if [[ -f /usr/share/applications/org.gnome.Terminal.desktop ]]; then
+    sed -i '/NoDisplay=true/d' /usr/share/applications/org.gnome.Terminal.desktop
+fi
 if [[ -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
     sed -i '/NoDisplay=true/d'  /usr/share/applications/gnome-system-monitor.desktop
 fi
