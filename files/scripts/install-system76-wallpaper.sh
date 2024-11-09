53711c227052c02e57f45e4ec643f6ce63b84de6
Rich Renomeron <rrenomeron@gmail.com>
2024-11-09 15:20:46 -0500

    fix: update s76 wallpaper link

--- files/scripts/install-system76-wallpaper.sh
+++ files/scripts/install-system76-wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -L https://system76.com/assets/downloads/wallpapers/System76-Wallpapers.zip > /tmp/System76-Wallpapers.zip
+curl -L https://system76.com/content/downloads/System76-Wallpapers.zip > /tmp/System76-Wallpapers.zip
 mkdir /usr/share/backgrounds/system76
 cd /usr/share/backgrounds/system76
 unzip /tmp/System76-Wallpapers.zip
