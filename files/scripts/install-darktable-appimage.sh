a3a3f1d25effd9f108776ea569e1c6680a2192fa
Rich Renomeron <rrenomeron+github@gmail.com>
2025-01-16 08:57:23 -0500

    fix: include darktable roboto font, fix symlink

--- files/scripts/install-darktable-appimage.sh
+++ files/scripts/install-darktable-appimage.sh
@@ -2,19 +2,20 @@
 set -euo pipefail
 
 DARKTABLE_VERSION=5.0.0
-DARKTABLE_SHASUM="d0061ac5a345c473d98f04388197afaee48e61b638db576ae1c88700cb8855cd  Darktable-5.0.0-x86_64.AppImage"
+DARKTABLE_APPIMAGE=Darktable-$DARKTABLE_VERSION-x86_64.AppImage
+DARKTABLE_SHASUM="d0061ac5a345c473d98f04388197afaee48e61b638db576ae1c88700cb8855cd  $DARKTABLE_APPIMAGE"
 
 
 curl -L \
-    https://github.com/darktable-org/darktable/releases/download/release-$DARKTABLE_VERSION/Darktable-$DARKTABLE_VERSION-x86_64.AppImage \
-    > /tmp/Darktable-$DARKTABLE_VERSION-x86_64.AppImage
+    https://github.com/darktable-org/darktable/releases/download/release-$DARKTABLE_VERSION/$DARKTABLE_APPIMAGE \
+    > /tmp/$DARKTABLE_APPIMAGE
 
 cd /tmp
 echo $DARKTABLE_SHASUM | sha256sum -c
 
-chmod ugo+x ./Darktable-$DARKTABLE_VERSION-x86_64.AppImage
-./Darktable-$DARKTABLE_VERSION-x86_64.AppImage --appimage-extract
-cp ./Darktable-$DARKTABLE_VERSION-x86_64.AppImage /usr/bin
-ln -s /usr/bin/darktable-$DARKTABLE_VERSION-x86_64.AppImage /usr/bin/darktable
+chmod ugo+x ./$DARKTABLE_APPIMAGE
+./$DARKTABLE_APPIMAGE --appimage-extract
+cp ./$DARKTABLE_APPIMAGE /usr/bin
+ln -s /usr/bin/$DARKTABLE_APPIMAGE /usr/bin/darktable
 cp squashfs-root/org.darktable.darktable.desktop /usr/share/applications
 
