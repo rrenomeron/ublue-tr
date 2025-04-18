33fd52216eddeb51a55f1081a26fe949c8f7bbe1
Rich Renomeron <rrenomeron+github@gmail.com>
2025-04-17 23:19:26 -0400

    feat: add accent icons extension

---
+++ files/scripts/install-accent-icons-gnome-extension.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# Get the Accent Icons extension to allow Nautilus icons to follow the Gnome
+# accent color
+
+# See https://github.com/taiwbi/gnome-accent-directories
+
+set -eou pipefail
+
+ACCENT_ICONS_EXTENSION_VERSION=15
+ACCENT_ICONS_EXTENSION_ID=accent-directories@taiwbi.com
+curl -L https://extensions.gnome.org/extension-data/accent-directoriestaiwbi.com.v$ACCENT_ICONS_EXTENSION_VERSION.shell-extension.zip \
+    > /tmp/accent-icons-extension.zip
+
+EXTENSION_DIR=/usr/share/gnome-shell/extensions/$ACCENT_ICONS_EXTENSION_ID
+mkdir -p $EXTENSION_DIR && cd $EXTENSION_DIR
+unzip /tmp/accent-icons-extension.zip
+
+
+
+
