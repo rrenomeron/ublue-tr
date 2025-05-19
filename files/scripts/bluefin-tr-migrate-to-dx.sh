6d5024a3d9ccb3bbdc9916832549c23f6d63a7d0
Rich Renomeron <rrenomeron+github@gmail.com>
2025-05-18 22:39:55 -0400

    fix: bluefin-tr migration, again

--- files/scripts/bluefin-tr-migrate-to-dx.sh
+++ files/scripts/bluefin-tr-migrate-to-dx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-cat - >> /usr/lib/systemd/system/bluefin-tr-migrate-to-dx.service << EOF
+cat - >> /usr/lib/systemd/system/tr-migrate-to-dx.service << EOF
 [Unit]
 Description=Migrate system from discontinued bluefin-tr image to bluefin-dx-tr
 Wants=network-online.target
-After=system-flatpak-setup.service
+After=network-online.target
 
 [Service]
 Type=oneshot
@@ -15,5 +15,5 @@ Exec=bootc switch ghcr.io/rrenomeron/bluefin-dx-tr:gts
 WantedBy=multi-user.target
 EOF
 
-# Uncomment after May 11, 2025
-systemctl enable bluefin-tr-migrate-to-dx.service
+# Apparently this fails unless we run it in a snippet
+# systemctl enable tr-migrate-to-dx.service
