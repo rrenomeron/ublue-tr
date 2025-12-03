42f10385605b11e64910818e77c0741118ccdbe6
Rich Renomeron <rrenomeron+github@gmail.com>
2025-12-02 23:31:32 -0500

    refactor: use bluebuild systemd module

--- files/scripts/bluefin-disable-rootful-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -n "$(systemctl list-unit-files | grep docker)" ]; then
-    echo "Disabling Rootful Docker"
-    systemctl disable docker.service docker.socket
-fi
--- files/scripts/bluefin-disable-starship.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-sed -i '/starship init bash/d'  /etc/bashrc
