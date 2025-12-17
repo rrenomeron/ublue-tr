ea74da8219c7f8c47dcf5361e34b2c115a6b5b2d
Rich Renomeron <rrenomeron+github@gmail.com>
2025-12-16 21:39:24 -0500

    fix: skip container policy updates for ublue-tr

--- files/scripts/add-bootc-images-sigstore-key.sh
+++ files/scripts/add-bootc-images-sigstore-key.sh
@@ -6,6 +6,11 @@
 
 set -eou pipefail
 
-jq  -f $(dirname "$0")/add-bootc-images-sigstore-key-$IMAGE_NAME.jq \
-    /etc/containers/policy.json > /tmp/policy.json
-cp /tmp/policy.json /etc/containers/policy.json
+if [ "$IMAGE_NAME" != "ublue-tr" ]; then
+    echo "Updating Container Policy for $IMAGE_NAME"
+    jq  -f $(dirname "$0")/add-bootc-images-sigstore-key-$IMAGE_NAME.jq \
+        /etc/containers/policy.json > /tmp/policy.json
+    cp /tmp/policy.json /etc/containers/policy.json
+else
+    echo "Not updating container policy for $IMAGE_NAME"
+fi
